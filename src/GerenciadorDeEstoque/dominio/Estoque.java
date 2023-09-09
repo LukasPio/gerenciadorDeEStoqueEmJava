c7194678f6bf59041685c74490edeaa67b00fab6
lucas <lucaspio.galvao@gmail.com>
2023-09-09 14:11:03 -0300

    implementation of scanner mechanic

--- src/GerenciadorDeEstoque/dominio/Estoque.java
+++ src/GerenciadorDeEstoque/dominio/Estoque.java
@@ -1,5 +1,6 @@
 package GerenciadorDeEstoque.dominio;
 
+import javax.swing.*;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
@@ -26,6 +27,87 @@ public class Estoque {
         this.produtos = new Produto[]{produto};
     }
 
+    public static void iniciarAplicacao() {
+        Scanner scanner = new Scanner(System.in);
+
+        Computador computador1 = new Computador("dell 5 inspiron", 5000, "Um belo notebook para te introduzir ao mundo da tecnologia");
+        Computador computador2 = new Computador("Ryzen 5", 3400, "Um notebook básico, para tarefas simples");
+
+        Jogo jogo1 = new Jogo("God of war 2018", 300, "Um jogo que mudou o rumo da franqui God of War");
+        Jogo jogo2 = new Jogo("Elden Ring", 200, "Um dos melhores jogos no estilo souls já feito");
+
+        Servico servico1 = new Servico("DEV front-end", 100, "Um serviço freelancer no qual o dev resolve um problema seu");
+        Servico servico2 = new Servico("Cozinheiro de luxo", 740, "Contrato semanal para ter um cozinheiro particular na sua casa");
+
+        Estoque estoque = new Estoque(computador1, "Estoque Atacadão");
+
+        estoque.adicionarNovoProdutoNoEstoque(computador2);
+        estoque.adicionarNovoProdutoNoEstoque(jogo1);
+        estoque.adicionarNovoProdutoNoEstoque(jogo2);
+        estoque.adicionarNovoProdutoNoEstoque(servico1);
+        estoque.adicionarNovoProdutoNoEstoque(servico2);
+
+        int option;
+
+        do {
+
+            System.out.println("Escolha uma das opções a seguir: \n\n" +
+                    "1- Incrementar um produto ao estoque\n" +
+                    "2- Decrementar um produto no estoque\n" +
+                    "3- Ver a lista de produtos pela classe\n" +
+                    "4- Ver a lista de produtos ordenados pelo preço\n" +
+                    "5- Consultar para saber se um produto está no estoque\n" +
+                    "6- Imprimir um relatório geral do estoque\n" +
+                    "7- Adicionar um produto no estoque\n" +
+                    "8- Remover um produto no estoque\n" +
+                    "9- Fechar aplicação");
+
+            option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    estoque.incremetarAoEstoque();
+                    break;
+                case 2:
+                    estoque.decrementarProduto();
+                    break;
+                case 3:
+                    System.out.println("Digite o nome da classe que busca");
+                    String nomeDaClasse = scanner.nextLine();
+                    estoque.buscarProdutoPelaClasse(nomeDaClasse);
+                    break;
+                case 4:
+                    System.out.println("Deseja ordenar em ordem rescente ou decrescente?");
+                    String ordem = scanner.nextLine();
+                    estoque.ordenarEImprimirProdutos(ordem);
+                    break;
+                case 5:
+                    System.out.println("Digite o nome do produto que procura");
+                    String nomeDoProduto = scanner.nextLine();
+                    estoque.consultarProduto(nomeDoProduto);
+                    break;
+                case 6:
+                    estoque.relatorioTotalDoEstoque(estoque);
+                    break;
+                case 7:
+                    estoque.adicionarNovoProdutoNoEstoque();
+                    break;
+                case 8:
+                    System.out.println("Digite o id do produto a ser removido");
+                    int idProduto = scanner.nextInt();
+                    estoque.removerUmProdutoDoEstoque(idProduto);
+                    break;
+                case 9:
+                    System.out.println("Fechando aplicação...");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+
+            }
+
+        } while (option != 9);
+    }
+
     public boolean verificarProdutoValido(Produto produto) {
         for (Produto umProduto : this.produtos) {
             if (umProduto == produto) {
@@ -48,8 +130,8 @@ public class Estoque {
 
     public Produto buscarPeloId() {
         Scanner scanner = new Scanner(System.in);
-        int id = scanner.nextInt();
         System.out.println("Digite a id do produto");
+        int id = scanner.nextInt();
         Produto produto;
 
         for (Produto umProduto : this.produtos) {
@@ -64,37 +146,73 @@ public class Estoque {
 
 
     public void incremetarAoEstoque() {
-
         Produto produto = buscarPeloId();
         Scanner scanner = new Scanner(System.in);
-        int quantidadeParaAdicao = scanner.nextInt();
         System.out.println("Digite a quantidade a ser adicionada");
+        int quantidadeParaAdicao = scanner.nextInt();
 
         if (verificarProdutoValido(produto)) {
-            produto.quantidadeEmEstoque += quantidadeParaAdicao;
-            System.out.println("Foram adicionados " + quantidadeParaAdicao + " de " + produto.nome + " no estoque");
-        }
-        else {
-            System.out.println("Esse produto não está incluso no estoque, primeiro adicione-o");
-        }
-    }
-
-    public void decrementarProduto() {
-
-        Produto produto = buscarPeloId();
-        Scanner scanner = new Scanner(System.in);
-        int quantidadeParaRemocao = scanner.nextInt();
-        System.out.println("Digite a quantidade a ser removida");
-
-        if (verificarProdutoValido(produto)) {
-            produto.quantidadeEmEstoque -= quantidadeParaRemocao;
-            System.out.println("Foram adicionados " + quantidadeParaRemocao + " de " + produto.nome + " no estoque");
+            int novaQuantidade = produto.getQuantidadeEmEstoque() + quantidadeParaAdicao;
+            produto.setQuantidadeEmEstoque(novaQuantidade);
+            System.out.println("Foram adicionados " + quantidadeParaAdicao + " de " + produto.getNome() + " no estoque");
         } else {
             System.out.println("Esse produto não está incluso no estoque, primeiro adicione-o");
         }
     }
 
-    public void adicionarNovoProdutoNoEstoque(Produto produto) {
+    public void decrementarProduto() {
+        Produto produto = buscarPeloId();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite a quantidade a ser removida");
+        int quantidadeParaRemocao = scanner.nextInt();
+
+        if (verificarProdutoValido(produto)) {
+            int novaQuantidade = produto.getQuantidadeEmEstoque() - quantidadeParaRemocao;
+            if (novaQuantidade >= 0) {
+                produto.setQuantidadeEmEstoque(novaQuantidade);
+                System.out.println("Foram removidos " + quantidadeParaRemocao + " de " + produto.getNome() + " no estoque");
+            } else {
+                System.out.println("Quantidade insuficiente em estoque para remover.");
+            }
+        } else {
+            System.out.println("Esse produto não está incluso no estoque, primeiro adicione-o");
+        }
+    }
+
+    public void adicionarNovoProdutoNoEstoque() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o tipo do produto:\n" +
+                "Servico, " +
+                "Jogo, " +
+                "Computador");
+        String tipoProduto = scanner.nextLine();
+        System.out.println("Digite o nome do produto");
+        String nomeProduto = scanner.nextLine();
+        System.out.println("Digite a descricao do produto");
+        String descricaoProduto = scanner.nextLine();
+        System.out.println("Digite o preço do produto");
+        double precoProduto = scanner.nextDouble();
+
+        Produto produto;
+
+        if (tipoProduto.equals("Servico") || tipoProduto.equals("servico")) {
+            Servico servico = new Servico(nomeProduto, precoProduto, descricaoProduto);
+            produto = servico;
+        }
+        else if (tipoProduto.equals("Computador") || tipoProduto.equals("computador")) {
+            Computador computador = new Computador(nomeProduto, precoProduto, descricaoProduto);
+            produto = computador;
+        }
+        else if (tipoProduto.equals("Jogo") || tipoProduto.equals("Jogo")) {
+            Jogo jogo = new Jogo(nomeProduto, precoProduto, descricaoProduto);
+            produto = jogo;
+        }
+        else {
+            System.out.println("Tipo de protudo inválido!");
+            produto = null;
+        }
 
         boolean produtoValido = !verificarProdutoValido(produto);
 
@@ -109,8 +227,21 @@ public class Estoque {
             produtosEmEstoqueAtualizado[this.produtos.length] = produto;
             this.produtos = produtosEmEstoqueAtualizado;
         }
-        else {
-            System.out.println("Já existe um produto desse ou esse não é um produto válido");
+    }
+
+    public void adicionarNovoProdutoNoEstoque(Produto produto) {
+        boolean produtoValido = !verificarProdutoValido(produto);
+
+        Produto[] produtosEmEstoqueAtualizado = new Produto[this.produtos.length + 1];
+
+        if (produtoValido) {
+            produto.setQuantidadeEmEstoque(1); // Configure a quantidade inicial para 1 ou o valor desejado.
+            for (int i = 0; i < this.produtos.length; i++) {
+                produtosEmEstoqueAtualizado[i] = this.produtos[i];
+            }
+
+            produtosEmEstoqueAtualizado[this.produtos.length] = produto;
+            this.produtos = produtosEmEstoqueAtualizado;
         }
     }
 
@@ -133,6 +264,34 @@ public class Estoque {
             listarProdutos();
         } else {
             System.out.println("O produto não existe ou não está presente no estoque");
+        }
+    }
+
+    public void removerUmProdutoDoEstoque(int idProduto) {
+
+        boolean produtoValido = false;
+
+        for (Produto produto: this.produtos) {
+            if (produto.getId() == idProduto) {
+                produtoValido = true;
+            }
+            if (produtoValido) {
+                Produto[] novoArray = new Produto[this.produtos.length - 1];
+                int indexNovoArray = 0;
+
+                for (int i = 0; i < this.produtos.length; i++) {
+                    if (this.produtos[i] != produto) {
+                        novoArray[indexNovoArray] = this.produtos[i];
+                        indexNovoArray++;
+                    }
+                }
+
+                this.produtos = novoArray;
+                System.out.println("O produto foi removido");
+                listarProdutos();
+            } else {
+                System.out.println("O produto não existe ou não está presente no estoque");
+            }
         }
     }
 
@@ -168,29 +327,21 @@ public class Estoque {
     }
 
 
-    public void relatorioTotalDoEstoque () {
+    public void relatorioTotalDoEstoque(Estoque estoque) {
+        int produtosDiferentesNoEstoque = estoque.produtos.length;
         int quantidadeDeProdutosNoEstoque = 0;
-        int produtosDiferentesNoEstoque = this.produtos.length;
         double valorTotalDoEstoque = 0;
 
-        for (Produto produto: this.produtos) {
-
-                if (produto != null) {
-                    quantidadeDeProdutosNoEstoque += produto.quantidadeEmEstoque;
-                }
-
-        }
-
-        for (Produto produto: this.produtos) {
+        for (Produto produto : estoque.produtos) {
             if (produto != null) {
+                quantidadeDeProdutosNoEstoque += produto.getQuantidadeEmEstoque();
                 valorTotalDoEstoque += (produto.getPreco() * produto.getQuantidadeEmEstoque());
             }
         }
 
         System.out.println("Quantidade de produtos diferentes no estoque: " + produtosDiferentesNoEstoque);
-        System.out.println("Quantidade total de produtos no esotoque: " + quantidadeDeProdutosNoEstoque);
+        System.out.println("Quantidade total de produtos no estoque: " + quantidadeDeProdutosNoEstoque);
         System.out.println("Valor total de todos os produtos do estoque: " + valorTotalDoEstoque);
-
     }
 
     public void buscarProdutoPorNome(String nome) {
--- src/GerenciadorDeEstoque/test/MainFile.java
+++ src/GerenciadorDeEstoque/test/MainFile.java
@@ -1,73 +1,10 @@
 package GerenciadorDeEstoque.test;
-import java.util.Scanner;
 import GerenciadorDeEstoque.dominio.*;
 
 public class MainFile {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-
-        Computador computador1 = new Computador("dell 5 inspiron", 5000, "Um belo notebook para te introduzir ao mundo da tecnologia");
-        Computador computador2 = new Computador("Ryzen 5", 3400, "Um notebook básico, para tarefas simples");
-
-        Jogo jogo1 = new Jogo("God of war 2018", 300, "Um jogo que mudou o rumo da franqui God of War");
-        Jogo jogo2 = new Jogo("Elden Ring", 200, "Um dos melhores jogos no estilo souls já feito");
-
-        Servico servico1 = new Servico("DEV front-end", 100, "Um serviço freelancer no qual o dev resolve um problema seu");
-        Servico servico2 = new Servico("Cozinheiro de luxo", 740, "Contrato semanal para ter um cozinheiro particular na sua casa");
-        Servico servico3 = new Servico("Cozinheiro de luxo", 740, "Contrato semanal para ter um cozinheiro particular na sua casa");
-
-
-        Estoque estoque = new Estoque(computador1, "Estoque Atacadão");
-
-        estoque.adicionarNovoProdutoNoEstoque(computador2);
-        estoque.adicionarNovoProdutoNoEstoque(jogo1);
-        estoque.adicionarNovoProdutoNoEstoque(jogo2);
-        estoque.adicionarNovoProdutoNoEstoque(servico1);
-        estoque.adicionarNovoProdutoNoEstoque(servico2);
-
-
-
-        int option;
-
-        do {
-
-            option = scanner.nextInt();
-            System.out.println("Escolha uma das opções a seguir: \n\n" +
-                    "1- Incrementar um produto ao estoque" +
-                    "2- Decrementar um produto no estoque" +
-                    "3- Ver a lista de produtos pela classe" +
-                    "4- Ver a lista de produtos ordenados pelo preço" +
-                    "5- Consultar para saber se um produto está no estoque" +
-                    "6- Imprimir um relatório geral do estoque" +
-                    "7- Adicionar um produto no estoque" +
-                    "8- Remover um produto no estoque" +
-                    "9- Fechar aplicação");
-
-            switch (option) {
-                case 1:
-                    estoque.incremetarAoEstoque();
-                case 2:
-                    estoque.decrementarProduto();
-                case 3:
-                    String nomeDaClasse = scanner.next();
-                    System.out.println("Digite o nome da classe que busca");
-                    estoque.buscarProdutoPelaClasse(nomeDaClasse);
-                case 4:
-                    String ordem = scanner.next();
-                    System.out.println("Deseja ordenar em ordem rescente ou decrescente?");
-                    estoque.ordenarEImprimirProdutos(ordem);
-                case 5:
-                    String nomeDoProduto = scanner.next();
-                    System.out.println("Digite o nome do produto que procura");
-                    estoque.consultarProduto(nomeDoProduto);
-                case 6:
-                    estoque.relatorioTotalDoEstoque();
-                case 7:
-
-            }
-
-        } while (option != 9);
+        Estoque.iniciarAplicacao();
 
     }
 }
